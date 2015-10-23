ec0d176060bcebe6aa594ac1c9672767fc766b91
Hideyo Yoshida <aquaviter@gmail.com>
2015-10-24 02:08:32 +0900

    first commit

--- index.js
+++ index.js
@@ -1,53 +1,19 @@
-var aws = require('aws-sdk');
-var elasticsearch = require('elasticsearch');
+var AWS = require('aws-sdk');
 
-var ES_INDEX = 'device'; // Elasticsearch index name
-var ES_TYPE = 'log'; // Elasticsearch index type name
-var ES_CLIENT = new elasticsearch.Client({
-    // host: 'search-factory-demo-sthekcqwii4rlks6log62zldsa.us-west-1.cloudsearch.amazonaws.com'
-    host: 'search-iothandson-s6vk5i2kztfkxryspanslantha.ap-northeast-1.es.amazonaws.com' //Elasticsearch URL:port
-});
-
-//start lambda function
 exports.handler = function(event, context) {
   console.log('Received event:');
-  // Import arguments
-	var device = event.device;
-	var sensor = event.sensor;
-  var timestamp = event.timestamp;
-	var value = event.value;
+  var value = event.value;
+  var iotdata = new AWS.IotData({endpoint: 'data.iot.ap-northeast-1.amazonaws.com'});
 
-  // Compose payload
-  var searchRecords = [];
-  var header = {
-    "index":{
-      "_index": ES_INDEX,
-      "_type": ES_TYPE,
-      "_id": 'sensor'
-    }
+  console.log(JSON.stringify(event));
+
+  var thingName = 'edison';
+  var params = {
+      'payload': '{"state": {"desired": {"led": "on"}}}',
+      'thingName': thingName
   };
-
-  var searchRecord = {
-    "device":device,
-    "sensor" : sensor,
-    "timestamp" : timestamp,
-		"value": value
-  };
-  searchRecords.push(header);
-  searchRecords.push(searchRecord);
-  console.log(searchRecords);
-
-  // Put a search record
-  ES_CLIENT.bulk({
-    "body": searchRecords
-  }, function(err, resp) {
-    if(err){
-      console.log(err);
-      context.done("error",err);
-    }else{
-      console.log("Success");
-      console.log(resp);
-      context.done(null,'success');
-    };
+  iotdata.updateThingShadow(params, function(err, data) {
+    if (err) context.fail(err, err.stack); // an error occurred
+    else context.succeed(data.payload);
   });
 };
